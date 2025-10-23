0c15d115db5b08834a770455ca90c8a2053e4056
die23 <die23@txstate.edu>
2025-10-23 14:43:21 -0500

    MM-68 Different pin error messages depending on login status
    The pin will display different messages when encountering an error (not logged in and no song playing but logged in)

--- flask_folder/static/js/map.js
+++ flask_folder/static/js/map.js
@@ -72,9 +72,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const res = await fetch(`/spotify/now_playing?lat=${lat}&lng=${lng}&_=${Date.now()}`);
     // Parse JSON rather than just getting ALL the text
     const data = await res.json();
-    if (!data.authorized || !data.playing) {
+    if(!data.authorized) {
       marker
-        .bindPopup(`<b>No song currently playing</b>
+        .bindPopup(`<b>You are not logged in!</b>
+          <br><small>Lat: ${lat.toFixed(5)}, Lng: ${lng.toFixed(5)}</small>`)
+        .openPopup();
+      return;
+    }
+     
+    if (!data.playing) {
+      marker
+        .bindPopup(`<b>No song currently playing!</b>
           <br><small>Lat: ${lat.toFixed(5)}, Lng: ${lng.toFixed(5)}</small>`)
         .openPopup();
       return;
